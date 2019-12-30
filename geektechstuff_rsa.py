c3fa057192acfdefa54ceeabe2bc270f4e719775
GeekTechDude <34283325+geektechdude@users.noreply.github.com>
2019-12-30 11:05:59 +0000

    Update geektechstuff_rsa.py
    Removed functions whilst making sure it works

--- geektechstuff_rsa.py
+++ geektechstuff_rsa.py
@@ -3,27 +3,54 @@
 
 from sympy import randprime, isprime
 
-def create_rsa_r_8():
-    # Generates an 8-bit key
-    p = 0
-    q = 0
-    r = 257
-    while r > 256:
+# Generates an 8-bit key
+p = 0
+q = 0
+r = 257
+while r > 256:
+    # 1st prime (p)
+    p = randprime(2,250)
+    # 2nd prime (q)
+    q = randprime(2,250)
+    if p == q:
         # 1st prime (p)
         p = randprime(2,250)
         # 2nd prime (q)
         q = randprime(2,250)
-        if p == q:
-            # 1st prime (p)
-            p = randprime(2,250)
-            # 2nd prime (q)
-            q = randprime(2,250)
-        # RSA modulus (R) of both primes (p * q)
-        r = p * q
-    # Euler's totient
-    e = (p-1)*(q-1)
-    print("Euler = ",e)
-    return(r)
+    # RSA modulus (R) of both primes (p * q)
+    r = p * q
+print("RSA(r) = ",r)
+# Euler's totient
+e = (p-1)*(q-1)
+print("Euler = ",e)
+# Public key - relative prime, greater than 1 and less than Euler's totient
+pub_key = randprime(2,e)
+print("Public Key: ",pub_key)
+# Private key generation
+d = 0
+while d < 1000000000:
+    if (pub_key * d) % e == 1:
+        break
+    d = d+1
+priv_key = d
+print("Private Key: ",priv_key)
+   
 
-rsa = create_rsa_r_8()
-print("RSA modulus (r) = ",rsa)
+# Trying above to encrypt ASCII A
+print("Converting A to its ASCII value")
+ascii = ord("A")
+print("ASCII value of A is: ",ascii)
+print("Attempting to encrypt A")
+m = ascii
+
+cipher_text = (m**pub_key)%r
+print("(",m,"**",pub_key,") %",r )
+print("Ciphered letter becomes ASCII:",cipher_text)
+  
+c = cipher_text
+decrypt_text = (c**priv_key)%r
+print("(",c,"**",priv_key,") %",r)
+print("Decrypted letter becomes ASCII:",decrypt_text)
+    
+if m == decrypt_text:
+    print("Success!!")
